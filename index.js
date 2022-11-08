71339c471b2843d9526f46ffde9cff7ea3b7d93e
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-11-09 01:18:33 +0600

    Mongo DB Setup with Local Server

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
-const port = 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
 
 // MiddleWate 
 app.use(express())
 app.use(cors())
+
+
+// Password : S6zUSsVfeRgJNPm8
+// UserName: taxAdvisor
+
+const uri = "mongodb+srv://taxAdvisor:S6zUSsVfeRgJNPm8@cluster0.qctdu57.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('taxAdvisor').collection('services');
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to Tax Advisor Server!')
