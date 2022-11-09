8c1da9eb7c75f29082166298b983aa1c9db680f9
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-11-09 20:36:21 +0600

    Service added system from client side

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // MiddleWate 
-app.use(express())
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 
 // Password : S6zUSsVfeRgJNPm8
@@ -24,6 +25,22 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: ObjectId(id) };
+            const services = await serviceCollection.find({}).toArray();
+            const service = services.find(abc => abc._id === id);
+            res.send(service);
+        });
+
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            // console.log(service);
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
         });
     }
     finally {
