f11b2d7d3ca1c26a6d0761166a317f87d95b3101
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-11-10 22:42:01 +0600

    JWT token verify function setUp

--- index.js
+++ index.js
@@ -16,6 +16,24 @@ app.use(express.json());
 const uri = "mongodb+srv://taxAdvisor:S6zUSsVfeRgJNPm8@cluster0.qctdu57.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('taxAdvisor').collection('services');
